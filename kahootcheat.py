544c8fcb43053d5a3fbf27dbf6e70e031497b5e0
antikahoot <82637086+antikahoot@users.noreply.github.com>
2021-05-04 10:42:59 +0300

    Update kahootcheat.py

--- kahootcheat.py
+++ kahootcheat.py
@@ -1,7 +1,7 @@
 import urllib.request as ur
 import json
 
-gameid = input("Enter game id: ")
+gameid = input("Enter game id:a0c57b82-6814-42a9-bdf4-d7a420e3e62d ")
 url = "https://play.kahoot.it/rest/kahoots/" + gameid
 q = json.loads(ur.urlopen(url).read())["questions"]
 colours_list = ["red", "blue", "yellow", "green"]
